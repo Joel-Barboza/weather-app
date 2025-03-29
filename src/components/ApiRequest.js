c5d15a91cc7b8f5cad5ffc664e1d947d09574930
Joel-Barboza <joelbp5631@gmail.com>
2025-03-28 22:04:15 -0600

    again running, but it needs changes after changing from onecall(not free now) api to weather and forecast apis

--- src/components/ApiRequest.js
+++ src/components/ApiRequest.js
@@ -2,28 +2,43 @@
 
 // Gets data from the API
 const getWeatherData = () => {
-	navigator.geolocation.getCurrentPosition((success) => {
-		let {latitude, longitude} = success.coords;
+    navigator.geolocation.getCurrentPosition((success) => {
+        let { latitude, longitude } = success.coords;
+        console.log(latitude, longitude);
 
-		fetch(`https://api.openweathermap.org/data/2.5/onecall?
+        fetch(`https://api.openweathermap.org/data/2.5/weather?
 			lat=${latitude}&
 			lon=${longitude}&
 			exclude=minutely&
 			units=metric&
 			lang=es&
 			appid=${import.meta.env.VITE_API_KEY}`).then(res => res.json()).then(data => {
-			currentData(data);
-			hourlyData(data)
-            dailyData(data)
-			console.log(data)
-		})
-		fetch(`https://api.openweathermap.org/geo/1.0/reverse?
-			lat=${latitude}&lon=${longitude}&lang=es&
-			appid=${import.meta.env.VITE_API_KEY}`).then(res => res.json()).then(location => {
-			locationName(location);
-		})
-	}, (error) => {
-        switch(error.code) {
+            console.log(data)
+            currentData(data);
+            let preloader = document.getElementById("preloader")
+            preloader.style.display = "none"
+        })
+
+        /*---------------
+        TODO:fix forecast, now it's 5 days/3 hours
+        ----------------*/
+        // fetch(`https://api.openweathermap.org/data/2.5/forecast?
+        // 	lat=${latitude}&
+        // 	lon=${longitude}&
+        // 	exclude=minutely&
+        // 	units=metric&
+        // 	lang=es&
+        // 	appid=${import.meta.env.VITE_API_KEY}`).then(res => res.json()).then(data => {
+        //     console.log(data)
+        //     hourlyData(data)
+        //     dailyData(data)
+        // })
+        fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&lang=es&appid=${import.meta.env.VITE_API_KEY}`)
+            .then(res => res.json()).then(location => {
+                locationName(location);
+            })
+    }, (error) => {
+        switch (error.code) {
             case error.PERMISSION_DENIED:
                 alert(`Tenemos un problema al cargar la página.\n
 Algunas posibles causas son:
@@ -68,85 +83,86 @@ const reload = () => {
 
 // Gets location name
 const locationName = (location) => {
-	let {name} = location[0];
-	let place = document.getElementById('place');
-	place.innerHTML =  `<i class="fa-solid fa-location-dot"></i> ${name}`;
+    console.log(location[0])
+    let { name } = location[0];
+    let place = document.getElementById('place');
+    place.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${name}`;
 }
 
 // Displays hourly data
 const hourlyData = (data) => {
     // used to show the corret hour depending on what date.getHours() value is and show the next 24 hours
-    let hoursArray = 
-    ["12:00 am", "1:00 am", "2:00 am", "3:00 am", "4:00 am" , "5:00 am" ,
-     "6:00 am" , "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am",
-     "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm" , "5:00 pm" , 
-     "6:00 pm" , "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm", "12:00 am"]
-	const date = new Date();
-	let hour = date.getHours()//gets current hour
+    let hoursArray =
+        ["12:00 am", "1:00 am", "2:00 am", "3:00 am", "4:00 am", "5:00 am",
+            "6:00 am", "7:00 am", "8:00 am", "9:00 am", "10:00 am", "11:00 am",
+            "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm",
+            "6:00 pm", "7:00 pm", "8:00 pm", "9:00 pm", "10:00 pm", "11:00 pm", "12:00 am"]
+    const date = new Date();
+    let hour = date.getHours()//gets current hour
     let hourForecast;
-	let hourForecastNum = 0; // index of the array of API data used
+    let hourForecastNum = 0; // index of the array of API data used
     let groupNum = 1; // gives a number to each data items group for a scroll efect
     let contHourlyData = document.getElementById('container_hourly-data')
     contHourlyData.innerHTML = "" // prevents duplicated appends 
 
-    for (let i = 0; i < 4;){
+    for (let i = 0; i < 4;) {
         let dataGroup = document.createElement("div") // create the item groups
         dataGroup.setAttribute("id", `dataGroup${groupNum}`)
         dataGroup.classList.add("dataGroup")
         contHourlyData.appendChild(dataGroup)
 
-        for (let i = 0; i < 6;){
+        for (let i = 0; i < 6;) {
             hourForecast = hoursArray[hour]; //uses a element of the array to display an hour
-            let {temp} = data.hourly[hourForecastNum]; // gets temperature from API data
-            let {icon} = data.hourly[hourForecastNum].weather[0]; // gets a icon depending on the ->
+            let { temp } = data.hourly[hourForecastNum]; // gets temperature from API data
+            let { icon } = data.hourly[hourForecastNum].weather[0]; // gets a icon depending on the ->
             // -> weather condition gotten from API data
             let selectGroup = document.getElementById(`dataGroup${groupNum}`) // Selects the group to ->
             // -> append the data item
             let dataSpan = document.createElement("span")
             dataSpan.classList.add("hourlyItem")
-            dataSpan.innerHTML = 
-            `<p class="p_hour-forecast">${hourForecast}</p>
+            dataSpan.innerHTML =
+                `<p class="p_hour-forecast">${hourForecast}</p>
             <div class="container_hourly-img">
                 <img class="img" src="http://openweathermap.org/img/w/${icon}.png" alt="${icon}.png"/>
             </div>
             <p>${Math.round(temp)}°C</p>`
             selectGroup.appendChild(dataSpan)
-            
+
             if (hour === 24) {
-                hour = 0;           
+                hour = 0;
             }
 
-            hour++;  
+            hour++;
             hourForecastNum++;
-            i++;  
+            i++;
         }
         groupNum++;
-        i++; 
+        i++;
     }
 
     // Removes the preloader before hourly data was loaded
-    let preloader = document.getElementById("preloader")
-    preloader.style.display = "none"
+    // let preloader = document.getElementById("preloader")
+    // preloader.style.display = "none"
 }
 
 const dailyData = (data) => {
-    let daysArray= ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
+    let daysArray = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
     const date = new Date();
     let days = date.getDay()
     let dayNum = 0;
     let dailyDataCont = document.getElementById("container_daily-data")
     dailyDataCont.innerHTML = ""
-    
+
     for (let i = 0; i <= 6; i++) {
         let nextDays = days;
-        let {max, min} = data.daily[dayNum].temp;
-        let {humidity, sunrise, sunset, wind_speed} = data.daily[dayNum]
-        let sr = new Date(sunrise * 1000).toLocaleTimeString("en-US", {timeStyle: "short"})
-        let ss = new Date(sunset * 1000).toLocaleTimeString("en-US", {timeStyle: "short"})
+        let { max, min } = data.daily[dayNum].temp;
+        let { humidity, sunrise, sunset, wind_speed } = data.daily[dayNum]
+        let sr = new Date(sunrise * 1000).toLocaleTimeString("en-US", { timeStyle: "short" })
+        let ss = new Date(sunset * 1000).toLocaleTimeString("en-US", { timeStyle: "short" })
         let dailyDataItem = document.createElement("div")
         dailyDataItem.classList.add("daily-data_item")
-        dailyDataItem.innerHTML = 
-        `<div class="daily-main-data">
+        dailyDataItem.innerHTML =
+            `<div class="daily-main-data">
             <p class="day_name">${daysArray[days]}:</p>
             <p class="day_minmax-weather">${Math.round(max)}°C / ${Math.round(min)}°C</p>
             <button class="arrow-btn btn-hide" id="Btn${dayNum}">
@@ -162,39 +178,39 @@ const dailyData = (data) => {
         dailyDataCont.appendChild(dailyDataItem)
 
         let btnHideSecunData = document.getElementById(`Btn${dayNum}`)
-        btnHideSecunData.addEventListener("click", function(e) {
+        btnHideSecunData.addEventListener("click", function (e) {
             secunData(this.id)
         });
 
         if (nextDays === 6) {
             days = 0;
-        }else if(nextDays !== 6) {
-            days++;    
+        } else if (nextDays !== 6) {
+            days++;
         };
         dayNum++;
-    };    
+    };
 }
 
 const currentData = (data) => {
-	let {/*humidity, pressure, sunrise, sunset, windspeed,*/ temp} = data.current;
-	let {description} = data.current.weather[0];
-	let {day, night} = data.daily[0].feels_like;
-		
-	let temperature = document.getElementById('temperature');
-	let currentWeather = document.getElementById('current-weather');
-	let dayNigthTemperature = document.getElementById('day/nigth-temperature');
+    let {/*humidity, pressure, sunrise, sunset, windspeed,*/ temp, feels_like } = data.main;
+    let { description } = data.weather[0];
+    // let { day, night } = data.daily[0].feels_like;
 
-	temperature.innerText = `${Math.round(temp)}°C`;
-	currentWeather.innerText =  `${description}`;
-	dayNigthTemperature.innerText =  `${Math.round(day)}°C / ${Math.round(night)}°C`;
+    let temperature = document.getElementById('temperature');
+    let currentWeather = document.getElementById('current-weather');
+    let dayNigthTemperature = document.getElementById('day/nigth-temperature');
+
+    temperature.innerText = `${Math.round(temp)}°C`;
+    currentWeather.innerText = `${description}`;
+    dayNigthTemperature.innerText = `${Math.round(feels_like)}°C`;
 
 }
 
-const secunData = (dayNum) => { 
+const secunData = (dayNum) => {
     const hiddenData = document.getElementById(`secunData${dayNum}`);
     const btnShowData = document.getElementById(`rotateArrow${dayNum}`);
     hiddenData.classList.toggle("change-height")
-    btnShowData.classList.toggle("rotate-arrow")  
+    btnShowData.classList.toggle("rotate-arrow")
 }
 
 
